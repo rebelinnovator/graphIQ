30f5cfdaa52da441c62a81ac3699183fb552ac1f
mapletree23 <apolo40423@gmail.com>
2021-09-15 21:33:53 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', 'ChartController@index');
-Route::get('/post','PostController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::get('/', 'ChartController@index');
+//Route::get('/post','PostController@index');
 
